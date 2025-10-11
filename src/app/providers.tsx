20946e2178a7fd60368c9f2198952b9ab7135b70
kapernikxd <54470526+kapernikxd@users.noreply.github.com>
2025-10-11 13:01:09 +0200

    Add global snackbar component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,14 @@ import { TranslationProvider } from '@/localization/TranslationProvider';
 import '@/localization/dictionaries';
 import { StoreProvider } from '@/stores/StoreProvider';
 import AuthPromptManager from '@/components/AuthPromptManager';
+import Snackbar from '@/components/ui/Snackbar';
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <TranslationProvider>
       <StoreProvider>
         <AuthPromptManager />
+        <Snackbar />
         {children}
       </StoreProvider>
     </TranslationProvider>
